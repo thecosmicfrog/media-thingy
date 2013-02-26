f1ddd9e3abb788c5d48c54432594835e6a1715be
Aaron Hastings <aaron@aaronhastings.me>
2013-02-26 19:51:38 +0000

    Cleaned up some code. Removed dummy values.

--- music/__init__.py
+++ music/__init__.py
@@ -8,18 +8,17 @@ audio_list = []
 def add_to_db(audio_files):
     for audio_file in audio_files:
         audio_file_id3 = eyed3.load(audio_file)
-#        print audio_file_id3.tag.title
 
         if not Artist.objects.filter(name=audio_file_id3.tag.artist).exists():
             ar1 = Artist(name=audio_file_id3.tag.artist)
             ar1.save()
     
         if not Album.objects.filter(title=audio_file_id3.tag.album).exists():
-            al1 = Album(title=audio_file_id3.tag.album, artist=ar1, year_released=1994, length='45:00', genre='Alternative')
+            al1 = Album(title=audio_file_id3.tag.album, artist=ar1)
             al1.save()
         
         if not Track.objects.filter(title=audio_file_id3.tag.title).exists():
-            t1 = Track(title=audio_file_id3.tag.title, album=al1, artist=ar1, length='3:23', url=audio_file)
+            t1 = Track(title=audio_file_id3.tag.title, album=al1, artist=ar1, url=audio_file)
             t1.save()
             print 'Added to DB: ' + audio_file_id3.tag.title
 
