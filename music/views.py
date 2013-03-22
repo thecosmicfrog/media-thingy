45c619592f3f2cd2b55e8052bebe516e36111944
Aaron Hastings <aaron@aaronhastings.me>
2013-03-22 20:06:34 +0000

    Added upload functionality. Users can now upload their own media files which then get moved to the media root and added to the database. The application does not need to be reloaded. Only supports single files at a time for now. Added upload button to nav bar.

--- music/views.py
+++ music/views.py
@@ -1,16 +1,84 @@
+import eyed3
+import os
+import shutil
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from music.models import Artist, Album, Track
-from MediaThingy.settings import SITE_URL
+from MediaThingy.settings import MEDIA_ROOT, MEDIA_URL, SITE_URL
 
 def base(request):
-    return render_to_response('base.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('base.html', locals())
 
 def home(request):
     return render_to_response('home.html', locals(), context_instance=RequestContext(request))
 
 def music(request):
     return render_to_response('music.html', locals(), context_instance=RequestContext(request))
+
+def upload(request):
+    return render_to_response('upload.html', locals(), context_instance=RequestContext(request))
+
+def upload_handler(request):
+    if request.method == 'POST':
+        f = request.FILES['upload']
+        filename = str(f)
+        f_ext = os.path.splitext(filename)[1]
+        
+        # If file is a compatible music file, build a filesystem structure for
+        # it (media/artist-name/album-name), then write it to that location and
+        # add details of the file to the database.
+        if f_ext in ['.mp3']:
+            with open(MEDIA_ROOT + str(f), 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+                
+                filename = eyed3.load(MEDIA_ROOT + filename)
+                dir_struct = MEDIA_ROOT + 'music' + '/' + filename.tag.artist + '/' + filename.tag.album + '/'
+                
+                try:
+                    if not os.path.isdir(dir_struct):
+                        os.makedirs(dir_struct)
+                    shutil.move(MEDIA_ROOT + str(f), dir_struct)
+                except OSError as ose:
+                    if ose.errno != 17:
+                        raise
+                    # Need to pass on Errno 17, due to race conditions caused
+                    # by making directories that already exist.
+                    pass
+                
+            if not Artist.objects.filter(name=filename.tag.artist).exists():
+                ar1 = Artist(name=filename.tag.artist)
+                ar1.save()
+    
+            if not Album.objects.filter(title=filename.tag.album).exists():
+                al1 = Album(title=filename.tag.album, artist=ar1)
+                al1.save()
+            
+            if not Track.objects.filter(title=filename.tag.title).exists():
+                t1 = Track(title=filename.tag.title, \
+                           album=al1, \
+                           artist=ar1, \
+                           fspath=dir_struct + str(f), \
+                           media_url=MEDIA_URL + (dir_struct + str(f)).split(MEDIA_ROOT)[1])
+                t1.save()
+                print 'Added to DB: ' + filename.tag.title
+            
+            return render_to_response('success.html', locals(), context_instance=RequestContext(request))            
+        
+        elif f_ext in ['.jpg']:
+            with open(MEDIA_ROOT + 'pictures/' + filename, 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+                
+                return render_to_response('success.html', locals(), context_instance=RequestContext(request)) 
+        
+        elif f_ext in ['.mp4']:
+            with open(MEDIA_ROOT + 'videos/' + filename, 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+                return render_to_response('success.html', locals(), context_instance=RequestContext(request)) 
+
+    return render_to_response('upload_failure.html', locals(), context_instance=RequestContext(request))
 
 def artists(request):
     artists = Artist.objects.order_by('name')
@@ -40,6 +108,7 @@ def artist_name_songs_track_title(request, artist_name, track_title):
     return render_to_response('music/artist/name/songs/track_title.html', locals(), context_instance=RequestContext(request))
 
 def artist_album_title(request, artist_name, album_title):
+    artist_name_split = artist_name.split('/albums')[0]
     album_tracks = Track.objects.filter(album__title=album_title)
     return render_to_response('music/artist/album_title.html', locals(), context_instance=RequestContext(request))
 
@@ -50,7 +119,7 @@ def albums(request):
 def album_title(request, album_title):
     album_tracks = Track.objects.filter(album__title=album_title).order_by('title')
     artist_set = Artist.objects.filter(album__title=album_title)
-    artist_name = [a.name for a in artist_set][0]
+    artist_name = [a.name for a in artist_set]
     return render_to_response('music/album/title.html', locals(), context_instance=RequestContext(request))
 
 def album_title_track_title(request, album_title, track_title):
